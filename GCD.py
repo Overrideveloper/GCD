eee5919249ddd9d77d739e1141c58ac6370dcd59
Banso D. Wisdom <bansowisdom@gmail.com>
2017-01-22 16:42:20 +0000

    Adding memory profiler

--- GCD.py
+++ GCD.py
@@ -1,8 +1,11 @@
 #Owner: Overrideveloper
 #Description: GCD Program 
 
-from memory_profiler import memory_usage
+
 from datetime import datetime
+import os
+import psutil
+
 startTime = datetime.now()
 
 #Creating a function that uses two parameters 
@@ -30,11 +33,8 @@ output = GreatestCommonDivisor (GreatestCommonDivisor (GreatestCommonDivisor(Gre
 print ("The Greatest Common Divisor of the five numbers is: ")
 
 print (output)
+process = psutil.Process(os.getpid())
 
 print ("The execution time is ",(datetime.now()- startTime))
-
-mem_usage = memory_usage(GreatestCommonDivisor)
-
-print('Memory usage (in chunks of .1 seconds): %s'% mem_usage)
-
-print('Maximum memory usage: %s'% max(mem_usage))
+print("Memory consumption in Kilobytes is: ")
+print(process.memory_info().rss / 1024)
